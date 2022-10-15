8a760838f94ba691fdbecd06d6915e6e72b5971d
wang11111 <991483114@qq.com>
2022-10-15 21:12:37 +0800

    组件移动对齐辅助线

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -44,7 +44,7 @@
                   </div>
                   <!-- 常用元素 -->
                   <div v-show="menuActive === 2" class="left-panel">
-                    <tools></tools>
+                    <tools :canvasObj="canvas"></tools>
                     <svgEl></svgEl>
                   </div>
                   <!-- 背景设置 -->
@@ -105,6 +105,10 @@ import attribute from '@/components/attribute.vue'
 // 功能组件
 import EventHandle from '@/utils/eventHandler'
 import hotkeys from '@/plugin/hotkeys'
+import MouseEvent from '@/plugin/MouseEvent'
+import initAligningGuidelines from '@/plugin/initAligningGuidelines'
+import initCenteringGuidelines from '@/plugin/initCenteringGuidelines'
+
 
 import { fabric } from 'fabric';
 
@@ -132,12 +136,20 @@ export default {
      this.$Spin.show();
   },
   mounted() {
-      this.canvas = canvas.c = new fabric.Canvas('canvas');
+      this.canvas = canvas.c = new fabric.Canvas('canvas',{
+        fireRightClick: true,
+        fireMiddleClick: true
+      });
+      initAligningGuidelines(this.canvas)
+      //initCenteringGuidelines(this.canvas)
+
       this.canvas.set('backgroundColor', '#fff')
       this.show = true
       this.$Spin.hide();
       event.init(canvas.c)
       hotkeys(canvas.c)
+      // 鼠标事件
+      MouseEvent(canvas.c)
       // 选中后的删除图标
       this.setRemoveIcon()
       this.setControlsStyle(fabric)
@@ -147,15 +159,15 @@ export default {
       var deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
       var img = document.createElement('img');
       img.src = deleteIcon;
-      fabric.Object.prototype.controls.deleteControl = new fabric.Control({
-        x: 0.5,
-        y: -1,
-        offsetY: 16,
-        cursorStyle: 'pointer',
-        mouseUpHandler: deleteObject,
-        render: renderIcon,
-        cornerSize: 24
-      });
+      // fabric.Object.prototype.controls.deleteControl = new fabric.Control({
+      //   x: 0.5,
+      //   y: -1,
+      //   offsetY: 16,
+      //   cursorStyle: 'pointer',
+      //   mouseUpHandler: deleteObject,
+      //   render: renderIcon,
+      //   cornerSize: 24
+      // });
 
       function deleteObject(eventData, transform) {
         var target = transform.target;
