846f98163a6d889303d8fcdb3dbc1136d1362bc4
wang11111 <991483114@qq.com>
2022-10-17 09:12:44 +0800

    itext

--- src/components/tools.vue
+++ src/components/tools.vue
@@ -2,7 +2,7 @@
   <div>
     <Divider plain orientation="left">常用元素</Divider>
     <div class="tool-box">
-      <span @click="drawTypeChange('text')">
+      <span @click="drawTypeChange('itext')">
         <svg t="1650875455324" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
           p-id="5401" width="26" height="26">
           <path
@@ -610,6 +610,23 @@ export default {
             ]
           })
           canvasObject.set('fill', grident);
+          break;
+        case "itext": //文本框
+          canvasObject = new fabric.IText("Hello world !", {
+            top: Math.min(mouseFrom.y, mouseTo.y),
+            left: Math.min(mouseFrom.x, mouseTo.x),
+            width: Math.abs(mouseFrom.x - mouseTo.x),
+            height: Math.abs(mouseFrom.y - mouseTo.y),
+
+            backgroundColor: '#FFFFFF',
+            fill: '#000000',
+            fontSize: 12,
+            lockScalingX: true,
+            lockScalingY: true,
+            hasRotatingPoint: false,
+            transparentCorners: false,
+            cornerSize: 7,
+          });
           break;
         case "text": //文本框
           this.textbox = new fabric.Textbox("", {
